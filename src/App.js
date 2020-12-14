e71bb6cc6129dfbb790ce572e5966c57f7565ff9
Justas Titovec <justas.titovec@yahoo.com>
2020-12-14 14:29:18 +0100

    default video

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends React.Component {
     selectedVideo: null,
   };
 
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
+
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
@@ -24,7 +28,10 @@ class App extends React.Component {
       },
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
   };
 
   onVideoSelect = (video) => {
