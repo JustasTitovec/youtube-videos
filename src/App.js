f671392337e395ec0ff924e365543cfea8fbd145
Justas Titovec <justas.titovec@yahoo.com>
2020-12-14 12:43:42 +0100

    fetching data from youtube api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import youtube from './apis/youtube.js';
+import VideoList from './components/VideoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    videos: [],
+  };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      q: term,
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
+  npm;
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 export default App;
